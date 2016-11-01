6042b948a8336d26faae62a4581bb6c9582ef6fe
Iain <iain@polevaultweb.com>
2016-11-01 11:08:20 +0000

    Initial commit

---
+++ src/Logger.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Polevaultweb\PHPUnit_WP_CLI_Runner;
+
+/**
+ * PHPUnit logger
+ */
+class Logger {
+
+	/**
+	 * @param string $message Message to write.
+	 *
+	 * @return string
+	 */
+	public function info( $message ) {
+		//print $message;
+	}
+
+	/**
+	 * @param $message
+	 *
+	 * @return mixed
+	 */
+	public function success( $message ) {
+		return $message;
+	}
+
+	/**
+	 * @param $message
+	 *
+	 * @return mixed
+	 */
+	public function warning( $message ) {
+		//print $message;
+	}
+
+	/**
+	 * @param $message
+	 *
+	 * @return mixed
+	 */
+	public function error( $message ) {
+		//print $message;
+	}
+
+	/**
+	 * @param $message_lines
+	 *
+	 * @return string
+	 */
+	public function error_multi_line( $message_lines ) {
+		$message = implode( "\n", $message_lines );
+
+		print $message;
+	}
+}
